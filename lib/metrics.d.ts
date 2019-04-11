23fcab34668b2ad072fa4dafac4b628af2c1cf24
Igor Ramadas <igor@devv.com>
2019-04-11 17:02:48 +0200

    Removed compiled lib.

--- lib/metrics.d.ts
+++
@@ -1,8 +0,0 @@
-/** Metrics container. */
-declare class Metrics {
-    private static _instance;
-    /** @hidden */
-    static readonly Instance: Metrics;
-}
-declare const _default: Metrics;
-export = _default;
